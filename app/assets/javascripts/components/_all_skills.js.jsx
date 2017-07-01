4769caa79d7b5489e7f090cd2cb99f6c75b7d615
mike vallano <mikevallano@gmail.com>
2017-07-01 09:50:12 -0500

    Add D of CRUD

--- app/assets/javascripts/components/_all_skills.js.jsx
+++ app/assets/javascripts/components/_all_skills.js.jsx
@@ -1,18 +1,23 @@
 var AllSkills = React.createClass({
 
+  handleDelete(id) {
+    this.props.handleDelete(id);
+  },
+
 
   render() {
 
     console.log(this.prop)
 
     var skills =  this.props.skills.map((skill) => {
-                  return  (
-                          <div key={skill.id}>
-                            <h3>{skill.name}</h3>
-                            <h5>{skill.details}</h5>
-                          </div>
-                          )
-                  });
+      return (
+        <div key={skill.id}>
+          <h3>{skill.name}</h3>
+          <h5>{skill.details}</h5>
+          <button onClick={this.handleDelete.bind(this, skill.id)}>Delete</button>
+        </div>
+      )
+    });
 
     return(
         <div>
